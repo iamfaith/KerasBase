6b293811486d56a79ed2a7f01f620504cbc035e2
unknown <zi_xian@xian.encs.concordia.ca>
2020-04-15 10:40:11 -0400

    add

--- DNN.py
+++ DNN.py
@@ -39,24 +39,27 @@ class DNN(BaseTrainer):
 
     def train(self):
         batch_size = 64
-        nb_epoch = 105
+        nb_epoch = 100
         if self.has_train:
             nb_epoch = nb_epoch - self.epoch
             print('new epoch', nb_epoch)
-            self.model.fit(self.X_train, self.y_train, batch_size=batch_size, nb_epoch=nb_epoch,
+            self.model.fit(self.X_train, self.y_train, batch_size=batch_size, epochs=nb_epoch,
                            callbacks=[self.checkpointer, self.csv_logger])
         else:
             # 1. define the network
-            model = Sequential()
-            model.add(Dense(1024, input_dim=41, activation='relu'))
-            model.add(Dropout(0.01))
-            model.add(Dense(1))
-            model.add(Activation('sigmoid'))
-            model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+            self.model = Sequential()
+            self.model.add(Dense(1024, input_dim=41, activation='relu'))
+            self.model.add(Dropout(0.01))
+            self.model.add(Dense(1))
+            self.model.add(Activation('sigmoid'))
+            self.model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-            model.fit(self.X_train, self.y_train, batch_size=batch_size, nb_epoch=nb_epoch,
+            self.model.fit(self.X_train, self.y_train, batch_size=batch_size, epochs=nb_epoch,
                       callbacks=[self.checkpointer, self.csv_logger])
-            model.save("./dnn1layer_model.hdf5")
+            self.model.save("./dnn1layer_model.hdf5")
+        score, acc = self.model.evaluate(self.X_test, self.y_test)    
+        print('Test score:', score)
+        print('Test accuracy', acc)
 
 
 if __name__ == '__main__':

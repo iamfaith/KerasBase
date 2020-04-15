5f9f953104416532d8f04388ad96467a8cda4755
xianzixiang <xianzixiang@wps.cn>
2020-04-14 23:20:20 -0400

    first

---
+++ DNN.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import numpy as np
+from common import *
+
+np.random.seed(1337)  # for reproducibility
+from sklearn.preprocessing import Normalizer
+from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras import Sequential
+from tensorflow.keras.layers import Dropout, Activation, Embedding
+from tensorflow.keras.layers import LSTM, SimpleRNN, GRU
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+
+
+class DNN(BaseTrainer):
+
+    @Decorator.log(True)
+    def load_data(self, **dict):
+        print(dict)
+        self.traindata = pd.read_csv(dict['train'], header=None)
+        self.testdata = pd.read_csv(dict['test'], header=None)
+        X = self.traindata.iloc[:, 1:42]
+        Y = self.traindata.iloc[:, 0]
+        C = self.testdata.iloc[:, 0]
+        T = self.testdata.iloc[:, 1:42]
+        trainX = np.array(X)
+        testT = np.array(T)
+        trainX.astype(float)
+        testT.astype(float)
+        scaler = Normalizer().fit(trainX)
+        trainX = scaler.transform(trainX)
+        scaler = Normalizer().fit(testT)
+        testT = scaler.transform(testT)
+
+        self.y_train = np.array(Y)
+        self.y_test = np.array(C)
+
+        self.X_train = np.array(trainX)
+        self.X_test = np.array(testT)
+
+    def train(self):
+        if self.has_train:
+            self.model.fit(self.X_train, self.y_train, epochs=5, batch_size=50,
+                           callbacks=[self.checkpointer, self.csv_logger])
+        else:
+            batch_size = 64
+            # 1. define the network
+            model = Sequential()
+            model.add(Dense(1024, input_dim=41, activation='relu'))
+            model.add(Dropout(0.01))
+            model.add(Dense(1))
+            model.add(Activation('sigmoid'))
+            model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+            model.fit(self.X_train, self.y_train, batch_size=batch_size, nb_epoch=100,
+                      callbacks=[self.checkpointer, self.csv_logger])
+            model.save("/dnn1layer_model.hdf5")
+
+
+if __name__ == '__main__':
+    dnn = DNN()
+    dnn.load_data(train='../kdd/binary/Training.csv', test='../kdd/binary/Testing.csv')
+    dnn.train()
---
+++ common.py
@@ -0,0 +1,58 @@
+import os
+import functools
+from tensorflow.keras.callbacks import CSVLogger
+from tensorflow.keras import callbacks
+import tensorflow as tf
+
+
+class Decorator(object):
+    @staticmethod
+    def log(is_debug=False):
+        def decorator(func):
+            @functools.wraps(func)
+            def wrapper(*args, **kw):
+                if is_debug:
+                    print('call %s():' % func)
+                    print('parameters:', args, kw)
+                return func(*args, **kw)
+
+            return wrapper
+
+        return decorator
+
+    # @staticmethod
+    # def test(decorated):
+    #     pass
+
+
+class BaseTrainer(object):
+    __suffix = ".hdf5"
+    __checkpoint = "{epoch:02d}-checkpoint" + __suffix
+    __cp_folder = os.path.abspath(os.getcwd()) + "/checkpoint"
+    cp_path = __cp_folder + "/" + __checkpoint
+    model_path = "./model"
+    __log_name = "./training_analysis.csv"
+
+    def __init__(self):
+        self.has_train = False
+        print(BaseTrainer.__cp_folder)
+        files = []
+        if os.path.exists(BaseTrainer.__cp_folder):
+            for f in os.listdir(BaseTrainer.__cp_folder):
+                if f.endswith(BaseTrainer.__suffix):
+                    self.has_train = True
+                    files.append(f)
+        else:
+            os.makedirs(BaseTrainer.__cp_folder)
+        files.sort(key=lambda f: int(filter(str.isdigit, f)), reverse=True)
+        self.csv_logger = CSVLogger(BaseTrainer.__log_name, separator=',', append=False)
+        self.checkpointer = callbacks.ModelCheckpoint(filepath=BaseTrainer.cp_path, verbose=1, save_best_only=True,
+                                                      monitor='loss')
+        if self.has_train:
+            from keras.models import load_model
+            print(files)
+            self.model = tf.keras.models.load_model(BaseTrainer.__cp_folder + "/" + files[0])
+
+
+if __name__ == '__main__':
+    base = BaseTrainer()
